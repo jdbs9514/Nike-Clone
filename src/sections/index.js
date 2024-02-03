eefd416348ff1943c704e87eb1a1989803645b32
Josue Benavides <jdbs9514@gmail.com>
2024-02-03 18:05:55 -0500

    set up repo

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Services from "./Services";
+import PopularProducts from "./PopularProducts";
+import Footer from "./Footer";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import CustomerReviews from "./CustomerReviews";
+
+export {
+  Hero,
+  Services,
+  PopularProducts,
+  Footer,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+  CustomerReviews
+}
